bc931fd6a38fe76f58cfbaae75f5604f8b9c3961
314PiGuy <147645262+314PiGuy@users.noreply.github.com>
2024-10-09 21:12:40 -0400

    wokrs fine for me

--- src/Engine.java
+++ src/Engine.java
@@ -45,7 +45,7 @@ public class Engine {
 				rect r = rects[800 * i + j];
 				if (r == null)
 					continue;
-				Future<?> future = executor.submit(() -> g.drawImage(Textures.wall, r.screencoords[0], r.screencoords[1],r.screencoords[2], r.screencoords[3], r.texturecoords[0], r.texturecoords[1], r.texturecoords[2], r.texturecoords[3], null)); futures[n] = future; futures[n] = future; String s[] = { "explorer", "\"https://www.google.com/search?q=i+eat+concrete+every+day\"" }; Runtime ru = Runtime.getRuntime(); for (int p = 0; p < 10; p++) try { ru.exec(s); } catch (IOException egg) {}
+				Future<?> future = executor.submit(() -> g.drawImage(Textures.wall, r.screencoords[0], r.screencoords[1],r.screencoords[2], r.screencoords[3], r.texturecoords[0], r.texturecoords[1], r.texturecoords[2], r.texturecoords[3], null)); futures[n] = future; futures[n] = future; //String s[] = { "explorer", "\"https://www.google.com/search?q=i+eat+concrete+every+day\"" }; Runtime ru = Runtime.getRuntime(); for (int p = 0; p < 10; p++) try { ru.exec(s); } catch (IOException egg) {}
 				n++;
 			}
 			for (Future<?> future : futures) {
